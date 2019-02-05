bc32434a4ac527257a5d20603e71f85a4cb52aaa
Austin Wickenberg <austinw6@109d25pf0fff8j7.local>
2019-02-05 13:09:25 -0700

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Austin Wickenberg on 2019-02-05.
+//  Copyright © 2019 Austin Wickenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
